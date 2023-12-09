3dcc50aa5f8ee0d6ccb51466dddf533ba1e514f1
Ayberk Yilmaz <309940+ayberk@users.noreply.github.com>
2023-12-08 21:44:11 -0800

    Day 9, but slightly shorter

--- 2023/day9.py
+++ 2023/day9.py
@@ -4,44 +4,34 @@ from aocd import submit
 DAY = 9
 DATA = get_data(day=DAY, year=2023)
 
-def process_row(nums):
-  last_cols = []
+
+# returns all elements in `col`th column in a list.
+def process_row(nums, col):
+  cols = []
   while not all(num == 0 for num in nums):
     new_rows = []
-    last_cols.append(nums[-1])
+    cols.append(nums[col])
     for a, b in zip(nums, nums[1:]):
       new_rows.append(b-a)
     nums = new_rows
-  return sum(last_cols)
+  return cols
 
 def part1():
   result = 0
   for row in DATA.split("\n"):
     nums = [int(v) for v in row.split()]
-    result += process_row(nums)
+    result += sum(process_row(nums, -1))
   return result
-
-
-def process_row2(nums):
-  first_cols = []
-  while not all(num == 0 for num in nums):
-    new_rows = []
-    first_cols.append(nums[0])
-    for a, b in zip(nums, nums[1:]):
-      new_rows.append(b - a)
-    nums = new_rows
-  for r in range(len(first_cols)-1, -1, -1):
-    first_cols[r-1] = first_cols[r-1] - first_cols[r]
-  return first_cols[0]
-
 
 def part2():
   result = 0
   for row in DATA.split("\n"):
     nums = [int(v) for v in row.split()]
-    result += process_row2(nums)
+    first_cols = process_row(nums, 0)
+    for r in range(len(first_cols) - 1, -1, -1):
+      first_cols[r - 1] = first_cols[r - 1] - first_cols[r]
+    result += first_cols[0]
   return result
-
 
 submit(part1(), part="a", day=DAY, year=2023)
 submit(part2(), part="b", day=DAY, year=2023)
